ae71354b319d6c5f39385d7dd962ed0be5b2019a
Beata Karolczak <karolczak.beata@gmail.com>
2018-03-10 02:05:35 +0100

    Add Google Auth scripts

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '846082509752-rd7v4q8ekbjfjoe47r390dsh0cgklosd.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '1l9ahCRG-Qfn6kp84Qg-I8lK',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
